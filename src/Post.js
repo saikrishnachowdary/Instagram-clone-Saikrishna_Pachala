53e8ae1b7982c6448ffad5ebf8d39e101e4adbba
saikrishna0003 <61234578+saikrishna0003@users.noreply.github.com>
2020-10-31 12:01:23 +0530

    day1

---
+++ src/Post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Post.css';
+
+const Post = () => {
+    return (
+        <>
+            <h3 className='Post__Username'> sai</h3> 
+            <img className='Post__Image' src='https://scontent.fhyd14-1.fna.fbcdn.net/v/t31.0-8/19092941_1339240899505436_8970907115632137381_o.jpg?_nc_cat=108&ccb=2&_nc_sid=a4a2d7&_nc_ohc=CqRB2MiP-zQAX83zddf&_nc_ht=scontent.fhyd14-1.fna&oh=746bd6d735009ee81d71162218d93e7a&oe=5FC36653'/>
+            <h4 className='Post__Description'><strong>saikrishna</strong> hi this is a post in rudrangi</h4>
+        </>
+    )
+}
+
+export default Post
